15d70c848763e18bd3e758c4761b9a7a75260d8c
anurag <anurag.chaudhari@noovosoft.com>
2023-08-08 15:38:26 +0530

    cancel order

--- src/main/kotlin/com/demo/shopping/controller/OrderController.kt
+++ src/main/kotlin/com/demo/shopping/controller/OrderController.kt
@@ -1,7 +1,9 @@
 package com.demo.shopping.controller
 
 import com.demo.shopping.DTO.CartDTO
+import com.demo.shopping.DTO.OrderDTO
 import com.demo.shopping.services.OrderService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,5 +15,9 @@ class OrderController(
     @PostMapping("/place-order")
     fun placeOrder(@RequestBody cart:CartDTO) {
         orderService.placeOrder(cart)
+    }
+    @PostMapping("/cancel-order")
+    fun cancelOrder(@RequestBody orderDTO: OrderDTO) {
+        orderService.cancelOrder(orderDTO)
     }
 }
--- src/main/kotlin/com/demo/shopping/repository/OrderRepository.kt
+++ src/main/kotlin/com/demo/shopping/repository/OrderRepository.kt
@@ -2,6 +2,14 @@ package com.demo.shopping.repository
 
 import com.demo.shopping.beans.Order
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface OrderRepository : JpaRepository<Order,Long>{
+    @Modifying
+    @Query(nativeQuery = true, value = """update orders set order_status = 'CANCELLED' 
+        where order_id =:orderId
+    """)
+    fun cancelOrder(@Param("orderId") orderId:Long)
 }
